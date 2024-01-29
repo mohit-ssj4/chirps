b936bdf68a761de5e26be81be2f3a53c20290330
Mohit Singh <mohit.singh2@talktalkplc.com>
2024-01-29 15:20:38 +0530

    showing the chirps in index page

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
@@ -12,4 +13,12 @@ class Chirp extends Model
     protected $fillable = [
         'message'
     ];
+
+    /**
+     * Method to connect a chirp to a user.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
